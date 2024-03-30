018d81a44c2033e79338fb68dc0f042f35a6d82c
Harsh Kumar <harshkumar_se21a14_23@dtu.ac.in>
2024-03-30 10:58:26 +0530

    Update README and CalculatorBloc

--- lib/presentation/screens/calculator/bloc/calculator_bloc.dart
+++ lib/presentation/screens/calculator/bloc/calculator_bloc.dart
@@ -8,9 +8,7 @@ class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
     on<NumberPressedEvent>((event, emit) {
       state.scrollController
           .jumpTo(state.scrollController.position.maxScrollExtent);
-      // int inputSize = state.inputExpression.length;
-      // String lastChar =
-      //     inputSize > 0 ? state.inputExpression[inputSize - 1] : "";
+
       String newInputExpression =
           state.inputExpression + event.number.toString();
       String newOutput = AppMethods.calculateResult(newInputExpression);
@@ -18,30 +16,6 @@ class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
         inputExpression: newInputExpression,
         output: newOutput,
       ));
-      // if (state.inputExpression == "") {
-      //   emit(CalculatorState(
-      //     inputExpression: event.number.toString(),
-      //     output: state.output,
-      //   ));
-      // } else if (!AppMethods.isOperator(lastChar)) {
-      //   String newOutput = AppMethods.calculateResult(newInputExpression);
-      //   emit(CalculatorState(
-      //     inputExpression: newInputExpression,
-      //     output: newOutput,
-      //   ));
-      // } else {
-      //   if (state.output == "") {
-      //     // String newInputExpression=state.inputExpression + event.number.toString();
-      //     String newOutput = AppMethods.calculateResult(newInputExpression);
-      //     // print("newOutput: $newOutput");
-      //     emit(CalculatorState(
-      //       inputExpression: newInputExpression,
-      //       output: newOutput,
-      //     ));
-      //   } else {
-
-      //   }
-      // }
     });
     on<OperatorPressedEvent>((event, emit) {
       state.scrollController
@@ -51,12 +25,6 @@ class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
       String lastChar =
           inputSize > 0 ? state.inputExpression[inputSize - 1] : "";
       if (inputSize <= 0) {
-        // if (op == '.') {
-        //   emit(CalculatorState(
-        //     inputExpression: ".",
-        //     output: "",
-        //   ));
-        // } else
         if (op == '-') {
           emit(CalculatorState(
             inputExpression: op,
@@ -95,14 +63,21 @@ class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
             output: newOutput,
           ));
         } else if (op == '=') {
-          String newOutput = AppMethods.calculateResult(state.inputExpression);
+          String newInputExpression = state.inputExpression;
+          if (AppMethods.isOperator(lastChar)) {
+            newInputExpression = AppMethods.removeLastChar(newInputExpression);
+          }
+          String newOutput = AppMethods.calculateResult(newInputExpression);
           emit(CalculatorState(
             inputExpression: newOutput,
             output: newOutput,
           ));
-        } else if (op == '.' && lastChar == '.') {
+        } else if (op == '.' &&
+            (lastChar == '.' || AppMethods.isOperator(lastChar))) {
+          String newInputExpression =
+              AppMethods.removeLastChar(state.inputExpression) + op;
           emit(CalculatorState(
-            inputExpression: state.inputExpression,
+            inputExpression: newInputExpression,
             output: state.output,
           ));
         } else if (op == '.') {
@@ -111,15 +86,16 @@ class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
             output: state.output,
           ));
         } else if (AppMethods.isOperator(lastChar)) {
-          // print("when there is already an operator");
           String newInputExpression =
               AppMethods.removeLastChar(state.inputExpression) + op;
+          if (lastChar == '-' && inputSize == 1) {
+            newInputExpression = '-';
+          }
           emit(CalculatorState(
             inputExpression: newInputExpression,
             output: state.output,
           ));
         } else {
-          // print("when there is no operator");
           emit(CalculatorState(
             inputExpression: state.inputExpression + event.operator,
             output: state.output,
